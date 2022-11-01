e0710dc8cbbd7a2c546f01faadef634f1ac22303
ksy133900 <tkdgns13578@gmail.com>
2022-11-01 16:58:38 +0900

    articles multi image

--- articles/forms.py
+++ articles/forms.py
@@ -1,11 +1,24 @@
 from django import forms
-from .models import Article, Comment
+from .models import *
+from django.forms import ClearableFileInput
 
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ["title", "content", "image"]
+        fields = [
+            "title",
+            "content",
+        ]
+
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ("image",)
+        widgets = {
+            "image": ClearableFileInput(attrs={"multiple": True}),
+        }
 
 
 class CommentForm(forms.ModelForm):
