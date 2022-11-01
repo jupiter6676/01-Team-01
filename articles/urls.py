2f7f8c25c9ea67ff14f9dee82b94620fcf896879
Subin <pomodoro5292@gmail.com>
2022-11-01 17:39:19 +0900

    create comment

--- articles/urls.py
+++ articles/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/update/", views.update, name="update"),
     path("<int:pk>/comments/", views.comment_create, name="comment_create"),
+    path('<int:article_pk>/comments/<int:comment_pk>/delete/', views.comment_delete, name='comment_delete'),
+    # path('<int:article_pk>/comments/<int:comment_pk>/update/', views.comment_update, name='comment_update'),
     path("<int:pk>/like/", views.like, name="like"),
 ]
