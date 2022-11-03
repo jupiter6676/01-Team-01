9c79951601d6f253972764ffbc703c562e26ba07
jupiter6676 <jupiter6676@naver.com>
2022-11-03 17:39:51 +0900

    reply

--- articles/urls.py
+++ articles/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path("<int:pk>/update/", views.update, name="update"),
     path("<int:pk>/like/", views.like, name="like"),
     path("<int:pk>/bookmark/", views.bookmark, name="bookmark"),
-    path("<int:article_pk>/comments/", views.comment_index, name="comment_index"),
     path("<int:pk>/comments/", views.comment_create, name="comment_create"),
     path("<int:article_pk>/comments/<int:comment_pk>/delete/", views.comment_delete, name='comment_delete'),
     path("<int:article_pk>/comments/<int:comment_pk>/update/", views.comment_update, name='comment_update'),
