b09e705e2080b12fa9ef2f0b381faa109418b1dd
jupiter6676 <jupiter6676@naver.com>
2022-11-04 11:39:36 +0900

    댓글 좋아요 비동기

--- articles/views.py
+++ articles/views.py
@@ -239,11 +239,20 @@ def comment_update(request, article_pk, comment_pk):
 def comment_like(request, article_pk, comment_pk):
     comment = get_object_or_404(Comment, pk=comment_pk)
     article = Article.objects.get(pk=article_pk)
+
     if request.user in comment.like_users.all():
         comment.like_users.remove(request.user)
+        is_liked = False
     else:
         comment.like_users.add(request.user)
-    return redirect("articles:detail", article_pk)
+        is_liked = True
+
+    data = {
+        "isLiked": is_liked,
+        "likeCount": comment.like_users.count(),
+    }
+
+    return JsonResponse(data)
 
 
 # 검색
