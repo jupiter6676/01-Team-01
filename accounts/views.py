09048de2c802d0d438096699681733cfe8df68e1
jupiter6676 <jupiter6676@naver.com>
2022-11-07 14:34:01 +0900

    북마크 모아보기

--- accounts/views.py
+++ accounts/views.py
@@ -55,10 +55,15 @@ def logout(request):
 def detail(request, pk):
     user = get_user_model().objects.get(pk=pk)
     articles = user.article_set.all()
-    print(articles)
     like_articles = user.like_articles.all()
-    print(like_articles)
-    context = {"user": user, "articles": articles, "like_articles": like_articles}
+    bookmark_articles = user.bookmark_articles.all()
+
+    context = {
+        "user": user, 
+        "articles": articles, 
+        "like_articles": like_articles,
+        "bookmark_articles": bookmark_articles,
+    }
     return render(request, "accounts/detail.html", context)
 
 
